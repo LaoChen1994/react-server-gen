dee8b9c139b787c9df61959a4eb0ea6d6e2a790b
chenyixiang <chenyixiang_cyx@youzan.com>
2021-08-14 14:47:40 +0800

    fix: 格式小调整

--- app/controller/home2/index.ts
+++ app/controller/home2/index.ts
@@ -4,6 +4,7 @@ export default {
   async getIndexHtml(ctx: IControllerCtx) {
     ctx.setState({ name: 'Jack2' });
     ctx.setState({ age: 20 });
+
     ctx.setState({
       price(price: number) {
         return price / 100;
